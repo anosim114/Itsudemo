db9d4ebefcac7c9adb88621905de0f2ab992cfda
Arnold Siemens <arnold_siemens@pm.me>
2019-10-26 11:28:15 +0200

    everything's working except firefox

--- main.rb
+++ main.rb
@@ -1,26 +1,52 @@
 require "socket"
+include Socket::Constants
 
 item_list = [
-    "Itomori.ogg",
+    "Jugemo.ogg",
     "Lemon.ogg",
     "A Town With An Ocean View.ogg",
+    "Itomori.ogg",
     "Girl who lept through time.ogg",
     "Merry.ogg",
     "Flower Dance.ogg"]
 
-server = TCPServer.new 5678
+# byte 14 is the page sequence number of an ogg pages
+psn = 14
+
+server = TCPServer.new 8080
 
 @connections = []
 
-session = server.accept
-@connections.push({"session": session, "i": 6})
-session.print "HTTP/1.1 200\r\n"
-session.print "content-type: audio/ogg\r\n"
-session.print "cache-control: no-cache, no-store\r\n"
-session.print "expires: Mon, 26 Jul 1997 05:00:00 GMT\r\n"
-session.print "x-content-type-options: nosniff\r\n"
-session.print "server: aaa\r\n"
-session.print "\r\n"
+Thread.new do
+    while session = server.accept do
+        puts "new connection"
+
+        req = session.gets || ""
+        if req.include? "favicon" 
+            session.print "HTTP/1.1 404\r\n\r\n"
+            session.close
+            next
+        end
+
+        session.print "HTTP/1.1 200\r\n"
+        session.print "content-type: audio/ogg\r\n"
+        session.print "cache-control: no-cache, no-store\r\n"
+        session.print "expires: Mon, 26 Jul 1997 05:00:00 GMT\r\n"
+        session.print "x-content-type-options: nosniff\r\n"
+        session.print "server: aaa\r\n"
+        session.print "\r\n"
+        
+        puts "sending info"
+        last_byte = 0
+        @info_packets.each do |i|
+            puts "info"
+            session.print i
+            last_byte = i.getbyte(psn)
+        end
+        last_byte += 1
+        @connections.push({"session": session, "i": last_byte})
+    end
+end
 
 local_list = []
 while true do
@@ -42,45 +68,43 @@ while true do
         @connections.each do |s|
             s[:i] = 0
         end
-        
+
         @info_packets = [
             file.read(packets.delete_at(0)),
+            file.read(packets.delete_at(0)),
             file.read(packets.delete_at(0))
         ]
 
-        @info_packets.each do |i|
-            session.print i
+        @info_packets.each do |buffer|
+            @connections.each do |c|
+                c[:session].print buffer
+            end
         end
 
-        # have a small cache of 5 seconds to send to the clients
-        @cache = [
-            file.read(packets.delete_at(0)),
-            file.read(packets.delete_at(0)),
-            file.read(packets.delete_at(0)),
-            file.read(packets.delete_at(0)),
-            file.read(packets.delete_at(0))
-        ]
+        sleep 1
         
-        @cache.each do |c|
-            session.print(c)
-        end
-
         while packets.length > 0 do
             buffer_size = packets.delete_at(0)
             buffer = file.read(buffer_size)
 
-            @cache.push(buffer)
-            @cache.shift()
-
-            # byte 14 is the page sequence number of an ogg pages
-            psn = 14
-            @connections.each do |s|
-                if s[:i] > 0
-                    buffer.setbyte(psn, s[:i])
-                    s[:i] += 1
+            @connections.each do |c|
+                orig = buffer.getbyte(psn)
+                Thread.new do
+                    begin
+                        if c[:i] > 0
+                            buffer.setbyte(psn, c[:i])
+                            puts "#{orig} becomes #{c[:i]}"
+                            c[:i] += 1
+                        else
+                            buffer.setbyte(psn, orig)
+                        end
+                        c[:session].print buffer
+                    rescue
+                        @connections.delete(c)
+                        puts "session closed"
+                    end
                 end
             end
-            session.print buffer
             sleep(1)
         end
     end
