c638f91441822506714c4f49ec1059383e34eb26
Arnold Siemens <arnold_siemens@pm.me>
2019-10-31 11:01:54 +0100

    added support for multiple streams

--- main.rb
+++ main.rb
@@ -1,130 +1,163 @@
+require "json"
+require "uri"
 require "socket"
 
-item_list = [
-    # "Jugemo.ogg",
-    "Flower Dance.ogg",
-    "Merry.ogg",
-    "Girl who lept through time.ogg",
-    "Lemon.ogg",
-    "A Town With An Ocean View.ogg",
-    "Itomori.ogg"
-]
+config_raw = File.read("config.json")
 
-# byte 14 is the page sequence number of an ogg pages
-psn = 18
-# okay, nice to know, but firefox does not like me to mess with the bytes
-# unless I update the checksum
+config = JSON.parse(config_raw)
 
-#start new server
-server = TCPServer.new("0.0.0.0", 8080)
+@conn = {}
+@default = config["default"]
 
-# array that will hold all current connections
-@connections = []
+# fist stream is default
+config["streams"].each do |s|
+    @conn[s["name"]] = []
+end
 
-# start new thread to accept new connections
+# start server on port
+puts "starting server on port: #{config["port"]}"
+server = TCPServer.new("0.0.0.0", config["port"])
+
 Thread.new do
     while session = server.accept do
-        puts "new connection: #{session}"
+        puts "\n----- NEW USER -----"
+        # test_uri.each do |uri|
 
-        # don't want to send data to favicon, sorry firefox
-        req = session.gets || ""
-        if req.include? "favicon" 
-            session.print "HTTP/1.1 404\r\n\r\n"
-            session.close
+        # Note: set 'success' to true, if a stream for req was found
+        # else I will send 404
+
+        req = session.gets "\r\n" || ""
+
+        # TODO: I don't know if this works like that
+        puts req
+        if req == ""
+            session.print "HTTP/1.1 404 NOT FOUND\r\n\r\n"
+            session.close()
             next
         end
 
-        # send basic http header
-        session.print "HTTP/1.1 200\r\n"
-        session.print "content-type: audio/ogg\r\n"
-        session.print "cache-control: no-cache, no-store\r\n"
-        session.print "x-content-type-options: nosniff\r\n"
-        session.print "server: aaa\r\n"
-        session.print "\r\n"
-        
-        # send first two ogg pages
-        # I think they hold information the audio players need?
-        # But I'm not certain, since I don't really know ogg well enough
-        last_byte = 0
-        @info_packets.each do |i|
-            session.print i
+        if !(req.include? " ")
+            session.print "HTTP/1.1 404 NOT FOUND\r\n\r\n"
+            session.close()
+            next
         end
 
-        session.print @cache
+        # TODO: any better way?
+        url = URI(req.split(" ")[1])
 
-        # add new connection to the array
-        # :i is the value of the page sequence number
-        # it will be set for the current song on each page
-        # as soon as the next song starts
-        # it is not necessary anymore and I set it to 0 and skip this connection therefore
-        @connections.push(session)
-    end
-end
-
-# copy of list of songs
-# currently hardcoded, most likely implement it different later
-local_list = []
-while true do
-    # copy global list of files to play if local one is empty
-    if local_list.length == 0
-        local_list.replace(item_list)
-    end
-
-    # get lastest file and delete it from list
-    name = local_list.delete_at(0)
-    puts "playing: #{name}"
-
-    # read whole file
-    file_size = File.size(name)
-    file = IO.read(name, file_size)
-
-    # check how big each page is, so that I will read later exactly one page (which is one second)
-    packets = []
-    file.split("OggS").each do |c|
-        packets.push(c.length + 4)
-    end
-
-    # first entry is empty, since the file starts with the keyword I use for splitting
-    # which produces ["", "data", "data", ...]
-    packets.delete_at(0)
-
-    # open the file again and start reading one page at the time
-    open(name) do |file|
-        # the first two packets in the file are important
-        # get the first two pages, they will be send to new conections
-        # since they contain information of the file
-        @info_packets = [
-            file.read(packets.delete_at(0)),
-            file.read(packets.delete_at(0))
-        ]
-
-        # send first two pages to all connections
-        @info_packets.each do |buffer|
-            @connections.each do |s|
-                s.print buffer
-            end
+        query = nil
+        if url.query != nil
+            query = URI.decode_www_form(url.query)
         end
+        puts url.to_s
 
-        # start sending pages, every second
-        while packets.length > 0 do
-            buffer_size = packets.delete_at(0)
-            buffer = file.read(buffer_size)
-
-            @cache = buffer.dup
-
-            @connections.each do |s|
-                Thread.new do
-                    begin
-                        s.print buffer
-                    rescue
-                        puts "session errored: #{s}"
-                        s.close
-                        @connections.delete(s)
+        success = false
+        stream = ""
+        if url.path == config["base_url"]
+            # check if the user wants a specific stream
+            # check if querys were provided
+            if query != nil
+                query.each do |q|
+                    # check if query with string of stream_param exists
+                    if q[0] == config["stream_param"]
+                        # check if a stream of that type exists
+                        if @conn.key?(q[1])
+                            # finally add him to stream
+                            puts "adding to stream '#{q[1]}'"
+                            @conn[q[1]].push(session)
+                            stream = q[1]
+                            success = true
+                        end
                     end
                 end
+            else
+                # put connection into default list
+                puts "default: adding to stream"
+                stream = @default
+                @conn[@default].push(session)
+                success = true
             end
-            sleep(1)
+        end
+        
+        # TODO: info packets
+        if success == true
+            @conn["#{stream}_packets"].each do |p|
+                session.print p
+            end
+
+            next
+        end
+        puts "send 404"
+        session.print "HTTP/1.1 404 NOT FOUND\r\n\r\n"
+        session.close()
+    end
+end
+
+config["streams"].each do |stream|
+    stream_config = "#{config["stream_config_location"]}#{stream["name"]}.json"
+    puts stream_config
+    Thread.new do
+        track_list = []
+        while true do
+            track_list = JSON.parse(File.read(stream_config)) if track_list.length == 0
+            name = track_list.delete_at(0)
+            file_location = "#{config["music_location"]}/#{name}"
+
+            puts "> #{stream["name"]} playing: #{name}"
+
+            file_size = File.size(file_location)
+            file = File.read(file_location, file_size)
+
+            packets = []
+            file.split("OggS").each do |c|
+                packets.push(c.length + 4)
+            end
+            # fist packet is empty
+            packets.delete_at(0)
+
+            open(file_location) do |file|
+
+                # get info packets
+                @conn["#{stream["name"]}_packets"] = [
+                    file.read(packets.delete_at(0)),
+                    file.read(packets.delete_at(0))
+                ]
+
+                # send info packets
+                @conn["#{stream["name"]}_packets"].each do |buffer|
+                    @conn[stream["name"]].each do |s|
+                        s.print buffer
+                    end
+                end
+
+                # send other packets
+                while packets.length > 0 do
+                    buffer_size = packets.delete_at(0)
+                    buffer = file.read(buffer_size)
+
+                    # TODO: implement cache packet
+        
+                    @conn[stream["name"]].each do |s|
+                        Thread.new do
+                            begin
+                                s.print buffer
+                            rescue
+                                puts "session errored"
+                                s.close
+                                @conn[stream["name"]].delete(s)
+                            end
+                        end
+                    end
+                    sleep(1)
+                end
+                
+            end
         end
     end
 end
-session.close
+
+# set api variables
+# like: current_listeners and so on
+while true
+    sleep 100
+end
