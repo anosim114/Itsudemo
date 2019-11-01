1019d4739fdf2ce7404c43335c901029b597f8c7
Arnold Siemens <arnold_siemens@pm.me>
2019-11-01 22:16:48 +0100

    + last

--- main.rb
+++ main.rb
@@ -20,80 +20,84 @@ server = TCPServer.new("0.0.0.0", config["port"])
 
 Thread.new do
     while session = server.accept do
-        puts "\n----- NEW USER -----"
-        # Note: set 'success' to true, if a stream for req was found
-        # else I will send 404
+        begin
+            puts "\n----- NEW USER -----"
+            # Note: set 'success' to true, if a stream for req was found
+            # else I will send 404
 
-        req = session.gets "\r\n" || ""
+            req = session.gets "\r\n" || ""
 
-        # TODO: I don't know if this works like that
-        puts req
-        if req == ""
-            session.print "HTTP/1.1 404 NOT FOUND\r\n\r\n"
-            session.close()
-            next
-        end
+            # TODO: I don't know if this works like that
+            puts req
+            if req == ""
+                session.print "HTTP/1.1 404 NOT FOUND\r\n\r\n"
+                session.close()
+                next
+            end
 
-        if !(req.include? " ")
-            session.print "HTTP/1.1 404 NOT FOUND\r\n\r\n"
-            session.close()
-            next
-        end
+            if !(req.include? " ")
+                session.print "HTTP/1.1 404 NOT FOUND\r\n\r\n"
+                session.close()
+                next
+            end
 
-        # TODO: any better way?
-        url = URI(req.split(" ")[1])
+            # TODO: any better way?
+            url = URI(req.split(" ")[1])
 
-        query = nil
-        if url.query != nil
-            query = URI.decode_www_form(url.query)
-        end
-        puts url.to_s
+            query = nil
+            if url.query != nil
+                query = URI.decode_www_form(url.query)
+            end
+            puts url.to_s
 
-        success = false
-        stream = ""
-        if url.path == config["base_url"]
-            # check if querys were provided
-            if query != nil
-                query.each do |q|
-                    # check if query with string of stream_param exists
-                    if q[0] == config["stream_param"]
-                        # check if a stream of that type exists
-                        if @conn.key?(q[1])
-                            # finally add him to stream
-                            puts "adding to stream '#{q[1]}'"
-                            @conn[q[1]].push(session)
-                            stream = q[1]
-                            success = true
+            success = false
+            stream = ""
+            if url.path == config["base_url"]
+                # check if querys were provided
+                if query != nil
+                    query.each do |q|
+                        # check if query with string of stream_param exists
+                        if q[0] == config["stream_param"]
+                            # check if a stream of that type exists
+                            if @conn.key?(q[1])
+                                # finally add him to stream
+                                puts "adding to stream '#{q[1]}'"
+                                @conn[q[1]].push(session)
+                                stream = q[1]
+                                success = true
+                            end
                         end
                     end
+                else
+                    # put connection into default list
+                    puts "default: adding to stream"
+                    stream = @default
+                    @conn[@default].push(session)
+                    success = true
                 end
-            else
-                # put connection into default list
-                puts "default: adding to stream"
-                stream = @default
-                @conn[@default].push(session)
-                success = true
             end
-        end
-        
-        if success == true
-            session.print "HTTP/1.1 200 OK\r\n"
-            session.print "Server: blessing/Itsudemo\r\n"
-            session.print "Content-Type: audio/ogg\r\n"
             
-            session.print "\r\n"
-            @conn["#{stream}_packets"].each do |p|
-                session.print p
-            end
+            if success == true
+                session.print "HTTP/1.1 200 OK\r\n"
+                session.print "Server: blessing/Itsudemo\r\n"
+                session.print "Content-Type: audio/ogg\r\n"
+                
+                session.print "\r\n"
+                @conn["#{stream}_packets"].each do |p|
+                    session.print p
+                end
 
+                next
+            end
+            puts "send 404"
+            session.print "HTTP/1.1 404 NOT FOUND\r\n"
+                session.print "Server: blessing/Itsudemo\r\n"
+                session.print "X-Go-To: https://miona.tk/stream\r\n"
+                session.print "\r\n"
+            session.close()
+        rescue
             next
         end
-        puts "send 404"
-        session.print "HTTP/1.1 404 NOT FOUND\r\n"
-            session.print "Server: blessing/Itsudemo\r\n"
-            session.print "X-Go-To: https://miona.tk/stream\r\n"
-            session.print "\r\n"
-        session.close()
     end
 end
 
@@ -121,6 +125,8 @@ config["streams"].each do |stream|
             open(file_location) do |file|
 
                 # get info packets
+                # ex: @conn["piano_packets"] are the info packages for the piano stream
+                #   every new stream needs
                 @conn["#{stream["name"]}_packets"] = [
                     file.read(packets.delete_at(0)),
                     file.read(packets.delete_at(0))
@@ -136,6 +142,7 @@ config["streams"].each do |stream|
                 # send other packets
                 while packets.length > 0 do
                     buffer_size = packets.delete_at(0)
+                    # TODO: don't read buffer_size on last one, but rather everything there is
                     buffer = file.read(buffer_size)
 
                     # TODO: implement cache packet
